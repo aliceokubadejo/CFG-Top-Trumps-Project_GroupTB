0c6fd200958d1aa849f2407fd37a6873bb5bd62e
aliceokubadejo <okubadejoalice@gmail.com>
2023-11-30 18:09:40 +0000

    Update CFG-Top-Trumps.py

--- CFG-Top-Trumps.py
+++ CFG-Top-Trumps.py
@@ -1,1 +1,15 @@
-print("Hello World")
+import random
+import requests
+def user_pokemon():
+    pokemon_card_number = random.randint(1,51)
+    url = f'https://pokeapi.co/api/v2/pokemon/{pokemon_card_number}/'
+    response = requests.get(url)
+    pokemon = response.json()
+
+
+    return {
+    'name': pokemon['name'],
+    'id': pokemon['id'],
+    'height': pokemon['height'],
+    'weight': pokemon['weight'],
+     }
