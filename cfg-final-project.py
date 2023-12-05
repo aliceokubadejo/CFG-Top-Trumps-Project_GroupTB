8ea79e5c564bcbc21fa8d9368f0710614cf7b745
aliceokubadejo <147999134+aliceokubadejo@users.noreply.github.com>
2023-12-05 20:06:38 +0000

    for loop implemented

--- cfg-final-project.py
+++ cfg-final-project.py
@@ -4,64 +4,67 @@ import requests
 player_count = 0
 computer_count = 0
 
+for win in range(3):
+    print("Round ", win+1)
 # defining the random and linking the API
-def random_pokemon():
-    pokemon_id = random.randint(1, 151)
-    url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
-    response = requests.get(url)
-    pokemon = response.json()
+    def random_pokemon():
+        pokemon_id = random.randint(1, 151)
+        url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_id}/"
+        response = requests.get(url)
+        pokemon = response.json()
 
-# making a dictionary of the stuff we want
-    return {
-        'name': pokemon['name'],
-        'id': pokemon['id'],
-        'weight': pokemon['weight'],
-        'height': pokemon['height'],
-    }
+    # making a dictionary of the stuff we want
+        return {
+            'name': pokemon['name'],
+            'id': pokemon['id'],
+            'weight': pokemon['weight'],
+            'height': pokemon['height'],
+        }
 
-chosen_pokemon = random_pokemon()
-opponent_pokemon = random_pokemon()
+    chosen_pokemon = random_pokemon()
+    opponent_pokemon = random_pokemon()
 
-#printing the player pokemon
+    #printing the player pokemon
+    def run():
 
-print(f"you have been given {chosen_pokemon['name']} as your pokemon")
-#drowzee hating club
-if chosen_pokemon['id'] == 96:
-    print(f"{chosen_pokemon['name']} is trash...yikes")
-    print("but we move")
-else:
-    print(f"{chosen_pokemon['name']} is pretty cool")
+        print(f"you have been given {chosen_pokemon['name']} as your pokemon")
+        #drowzee hating club
+    if chosen_pokemon['id'] == 96:
+        print(f"{chosen_pokemon['name']} is trash...yikes")
+        print("but we move")
+    else:
+        print(f"{chosen_pokemon['name']} is pretty cool")
 
-print(f"{chosen_pokemon['name']} has the ID number of {chosen_pokemon['id']}")
-print(f"{chosen_pokemon['name']} weighs {chosen_pokemon['weight']} lbs")
-print(f"{chosen_pokemon['name']} height is {chosen_pokemon['height']}")
+    print(f"{chosen_pokemon['name']} has the ID number of {chosen_pokemon['id']}")
+    print(f"{chosen_pokemon['name']} weighs {chosen_pokemon['weight']} lbs")
+    print(f"{chosen_pokemon['name']} height is {chosen_pokemon['height']}")
 
-#asking for stat choice
+    #asking for stat choice
 
-stat_choice = input("which stat would you like to use? ")
+    stat_choice = input("which stat would you like to use? ")
 
-#opposing pokemon
+    #opposing pokemon
 
-chosen_stat = chosen_pokemon[stat_choice]
-opponent_stat = opponent_pokemon[stat_choice]
-
-print(f"your opponent has been given {opponent_pokemon['name']}")
-print(f'their {stat_choice} is {opponent_stat}')
+    chosen_stat = chosen_pokemon[stat_choice]
+    opponent_stat = opponent_pokemon[stat_choice]
 
 
-if chosen_stat > opponent_stat:
-    player_count =+1
-    print("Congrats! Your pokemon trumped the computer's !! Winner winner chicken dinner!")
-elif chosen_stat < opponent_stat:
-     computer_count =+2
-    print("Loser!")
-else:
-    print("It's a draw!")
-
-#naming rounds 
-#allowing player to decide to continue playing
-#looping to accomodate additional rounds (may need to create separate function)
-#creating condition where play stops at best of 5
+    print(f"your opponent has been given {opponent_pokemon['name']}")
+    print(f'their {stat_choice} is {opponent_stat}')
 
 
-#presentation - run the code then talk through code + # commentary 
+    if chosen_stat > opponent_stat:
+        player_count =+1
+        print("Congrats! Your pokemon trumped the computer's !! Winner winner chicken dinner!")
+    elif chosen_stat < opponent_stat:
+        computer_count =+1
+        print("Loser!")
+    else:
+        print("It's a draw!")
+
+    print(player_count)
+    print(computer_count)
+    #naming rounds
+
+
+
